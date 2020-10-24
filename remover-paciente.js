675967e96bbbc9d1c166844575c28959948386a2
Matheus Manoel de Souza Santos <matheusm.souza.santos@gmail.com>
2020-10-24 13:00:15 -0300

    Add files via upload

---
+++ remover-paciente.js
@@ -0,0 +1,29 @@
+var pacientes= document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
+tabela.addEventListener("dblclick", function(event){
+
+  event.target.parentNode.classList.add("fadeOut")
+
+  setTimeout(function() {
+    event.target.parentNode.remove();
+  }, 1000);
+    //var alvoEvento =  event.target;
+    //var paiDoAlvo = alvoEvento.parentNode // TR = paciente = remover
+    //paiDoAlvo.remove();
+})
+
+
+
+
+
+
+
+
+
+//pacientes.forEach(function(paciente){
+  //paciente.addEventListener("dblclick",function(){
+      //console.log("Fui clicado com um duplo clique");
+    //  this.remove();
+//  });
+
+//});
